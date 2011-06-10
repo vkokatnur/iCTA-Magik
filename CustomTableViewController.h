0e321c5907c79789ef074891d0865ca9bd499a90
Vijay Kokatnur <kokatnur.vijay@gmail.com>
2011-06-10 03:24:16 -0500

    first commit

---
+++ CustomTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewController.h
+//  CTA
+//
+//  Created by Vijay Kokatnur on 5/19/11.
+//  Copyright 2011 Nistantrii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CustomTableViewController : UIViewController {
+    
+}
+
+@end
---
+++ StopSetupViewController.h
@@ -0,0 +1,16 @@
+//
+//  StopSetupViewController.h
+//  CTA
+//
+//  Created by Vijay Kokatnur on 5/27/11.
+//  Copyright 2011 Nistantrii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface StopSetupViewController : UIViewController {
+    
+}
+
+@end
